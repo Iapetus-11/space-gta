7e2b57708b94316fade265fd6058a6ae06b0f676
Milo Weinberg <iapetus011@gmail.com>
2025-04-11 23:40:19 -0400

    Improve physics + add chasers

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,233 @@
-use bevy::{input::keyboard, prelude::*};
-
-#[derive(Component, Deref, DerefMut)]
-struct Velocity(Vec2);
+use bevy::{color::palettes::tailwind, math::VectorSpace, prelude::*};
 
 #[derive(Component)]
-struct Vehicle {}
+struct Velocity {
+    max: Vec2,
+    val: Vec2,
+}
+
+impl std::ops::Deref for Velocity {
+    type Target = Vec2;
+
+    fn deref(&self) -> &Self::Target {
+        &self.val
+    }
+}
+
+impl std::ops::DerefMut for Velocity {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.val
+    }
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct Acceleration(Vec2);
+
+#[derive(Component)]
+struct Drag {
+    recommend: f32,
+    actual: f32,
+}
+
+#[derive(Component)]
+struct PlayerMarker;
+
+#[derive(Bundle)]
+struct PlayerVehicleBundle {
+    marker: PlayerMarker,
+    velocity: Velocity,
+    acceleration: Acceleration,
+    drag: Drag,
+    transform: Transform,
+    mesh: Mesh2d,
+    mesh_material: MeshMaterial2d<ColorMaterial>,
+}
+
+impl PlayerVehicleBundle {
+    fn new(mesh: Mesh2d, mesh_material: MeshMaterial2d<ColorMaterial>) -> Self {
+        Self {
+            marker: PlayerMarker,
+            velocity: Velocity { max: Vec2::new(700.0, 700.0), val: Vec2::ZERO },
+            acceleration: Acceleration(Vec2::ZERO),
+            drag: Drag { recommend: 1.0, actual: 0.0 },
+            transform: default(),
+            mesh,
+            mesh_material,
+        }
+    }
+}
+
+#[derive(Component)]
+struct ChaserMarker;
+
+#[derive(Bundle)]
+struct ChaserVehicleBundle {
+    marker: ChaserMarker,
+    velocity: Velocity,
+    acceleration: Acceleration,
+    transform: Transform,
+    mesh: Mesh2d,
+    mesh_material: MeshMaterial2d<ColorMaterial>,
+}
+
+impl ChaserVehicleBundle {
+    fn new(mesh: Mesh2d, mesh_material: MeshMaterial2d<ColorMaterial>) -> Self {
+        Self {
+            marker: ChaserMarker,
+            velocity: Velocity { max: Vec2::new(680.0, 680.0), val: Vec2::ZERO },
+            acceleration: Acceleration(Vec2::ZERO),
+            transform: default(),
+            mesh,
+            mesh_material,
+        }
+    }
+}
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn(Camera2d);
-
     commands.spawn((
-        Vehicle {},
-        Mesh2d(meshes.add(Circle::new(25.0))),
-        MeshMaterial2d(materials.add(Color::linear_rgb(1.0, 0.0, 0.9))),
-        Transform {
-            translation: Vec3::new(0.0, 0.0, 0.0),
-            ..default()
-        },
-        Velocity(Vec2::new(0.0, 0.0)),
+        PlayerVehicleBundle::new(
+            Mesh2d(meshes.add(Circle::new(25.0))),
+            MeshMaterial2d(materials.add(Color::from(tailwind::TEAL_500))),
+        ),
+        Camera2d,
+        Projection::from(OrthographicProjection {
+            scale: 2.0,
+            ..OrthographicProjection::default_2d()
+        }),
     ));
+
+    commands.spawn(ChaserVehicleBundle::new(
+        Mesh2d(meshes.add(Circle::new(25.0))),
+        MeshMaterial2d(materials.add(Color::from(tailwind::RED_500))),
+    ));
+
+    for idx in 0..100 {
+        commands.spawn((
+            Mesh2d(meshes.add(Rectangle::new(40.0, 20.0))),
+            MeshMaterial2d(materials.add(Color::from(tailwind::PURPLE_800))),
+            Transform::default().with_translation(Vec3::new(100.0 * idx as f32, 0.0, 0.0)),
+        ));
+    }
 }
 
-fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time: Res<Time>) {
+fn apply_velocity(
+    mut query: Query<(&mut Transform, &Velocity)>,
+    time: Res<Time>,
+) {
     for (mut transform, velocity) in &mut query {
         transform.translation.x += velocity.x * time.delta_secs();
         transform.translation.y += velocity.y * time.delta_secs();
     }
 }
 
+fn apply_drag(mut query: Query<(&mut Velocity, &Drag)>, time: Res<Time>) {
+    for (mut velocity, drag) in &mut query {
+        let drag = 1.0 + drag.actual * time.delta_secs();
+
+        velocity.x /= drag;
+        velocity.y /= drag;
+    }
+}
+
+fn apply_acceleration(mut query: Query<(&mut Velocity, &Acceleration)>, time: Res<Time>) {
+    for (mut velocity, acceleration) in &mut query {
+        **velocity += **acceleration * time.delta_secs();
+        **velocity = velocity.clamp(-velocity.max, velocity.max)
+    }
+}
+
 fn player_input(
     kb: Res<ButtonInput<KeyCode>>,
-    mut vehicle_velocity: Single<&mut Velocity, With<Vehicle>>,
+    player_vehicle_velocity_and_drag: Single<
+        (&Velocity, &mut Acceleration, &mut Drag),
+        With<PlayerMarker>,
+    >,
 ) {
-    const MAX_VEL: f32 = 200.0;
-    let sensitivity: f32 = 2.0;
+    const SENSITIVITY: f32 = 1000.0;
+    const SENSITIVITY_REVERSE_BOOST: f32 = 2.0;
 
-    if kb.pressed(KeyCode::ArrowUp) {
-        vehicle_velocity.y += sensitivity;
+    let (player_vehicle_vel, mut acceleration, mut player_vehicle_drag) =
+        player_vehicle_velocity_and_drag.into_inner();
+
+    **acceleration = Vec2::ZERO;
+    player_vehicle_drag.actual = player_vehicle_drag.recommend;
+
+    if kb.any_pressed([KeyCode::ArrowUp, KeyCode::KeyW]) {
+        let sensitivity_modifier = match player_vehicle_vel.y {
+            ..-50.0 => SENSITIVITY_REVERSE_BOOST,
+            _ => 1.0,
+        };
+        acceleration.y = SENSITIVITY * sensitivity_modifier;
+        player_vehicle_drag.actual = 0.0;
     }
 
-    if kb.pressed(KeyCode::ArrowRight) {
-        vehicle_velocity.x += sensitivity;
+    if kb.any_pressed([KeyCode::ArrowRight, KeyCode::KeyD]) {
+        let sensitivity_modifier = match player_vehicle_vel.x {
+            ..-50.0 => SENSITIVITY_REVERSE_BOOST,
+            _ => 1.0,
+        };
+        acceleration.x = SENSITIVITY * sensitivity_modifier;
+        player_vehicle_drag.actual = 0.0;
     }
 
-    if kb.pressed(KeyCode::ArrowDown) {
-        vehicle_velocity.y -= sensitivity;
+    if kb.any_pressed([KeyCode::ArrowDown, KeyCode::KeyS]) {
+        let sensitivity_modifier = match player_vehicle_vel.y {
+            -50.0.. => SENSITIVITY_REVERSE_BOOST,
+            _ => 1.0,
+        };
+        acceleration.y = -SENSITIVITY * sensitivity_modifier;
+        player_vehicle_drag.actual = 0.0;
     }
 
-    if kb.pressed(KeyCode::ArrowLeft) {
-        vehicle_velocity.x -= sensitivity;
+    if kb.any_pressed([KeyCode::ArrowLeft, KeyCode::KeyA]) {
+        let sensitivity_modifier = match player_vehicle_vel.x {
+            -50.0.. => SENSITIVITY_REVERSE_BOOST,
+            _ => 1.0,
+        };
+        acceleration.x = -SENSITIVITY * sensitivity_modifier;
+        player_vehicle_drag.actual = 0.0;
     }
+}
 
-    let clamped =
-        vehicle_velocity.clamp(Vec2::new(-MAX_VEL, -MAX_VEL), Vec2::new(MAX_VEL, MAX_VEL));
-    vehicle_velocity.x = clamped.x;
-    vehicle_velocity.y = clamped.y;
+fn update_chasers(
+    mut chasers: Query<(&Velocity, &Transform, &mut Acceleration), With<ChaserMarker>>,
+    player_trans: Single<&Transform, With<PlayerMarker>>,
+) {
+    const MAX_ACCELERATION: Vec2 = Vec2::new(900.0, 900.0);
+
+    for (velocity, transform, mut acceleration) in chasers.iter_mut() {
+        let distance = transform.translation - player_trans.translation;
+        let target = Vec2::new(-650.0 * distance.x.signum(), -650.0 * distance.y.signum());
+
+        **acceleration = (target - **velocity).clamp(-MAX_ACCELERATION, MAX_ACCELERATION);
+
+        // vel.x += (target_x - vel.x).clamp(-9.0, 9.0);
+        // vel.y += (target_y - vel.y).clamp(-9.0, 9.0);
+
+        // if vel.x.abs() < 4.0 {
+        //     vel.x = 0.0;
+        // } else {
+        //     // vel.x /= 1.1;
+        // }
+
+        // if vel.y.abs() < 4.0 {
+        //     vel.y = 0.0;
+        // } else {
+        //     // vel.y /= 1.1;
+        // }
+    }
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (setup,))
-        .add_systems(Update, (apply_velocity, player_input))
+        .add_systems(FixedUpdate, (player_input, apply_acceleration, apply_drag, apply_velocity))
+        .add_systems(Update, update_chasers)
+        .insert_resource(Time::<Fixed>::from_hz(64.0))
         .run();
 }
